24e265d0ba151c376801c01b76de9e249c4ddcdc
Thavarajan <Thavarajan@gmail.com>
2017-02-14 05:18:15 +0530

    serverconfig file added for  server workout

---
+++ tools/config/server.Config.ts
@@ -0,0 +1,54 @@
+import { argv } from 'yargs';
+import { SeedAdvancedConfig } from './seed-advanced.config';
+import { BUILD_TYPES } from './seed.config';
+
+export class ServerConfig extends SeedAdvancedConfig {
+
+  /**
+   * The directory where the server files are located.
+   * The default directory is `server`.
+   * @type {string}
+   */
+  APP_SERVER = argv['server'] || 'server';
+
+  /**
+   * The base folder of the server source files.
+   * @type {string}
+   */
+  APP_SERVER_SRC = `src/${this.APP_SERVER}`;
+
+  /**
+   * The folder for built server files in the `dev` environment.
+   * @type {string}
+   */
+  DEV_SERVER_DEST = `${this.DIST_DIR}/dev/${this.APP_SERVER}`;
+
+  /**
+   * The folder for the built server files in the `prod` environment.
+   * @type {string}
+   */
+  PROD_SERVER_DEST = `${this.DIST_DIR}/prod/${this.APP_SERVER}`;
+
+
+  /**
+   * The folder for the built files in the `prod` environment.
+   * @type {string}
+   */
+  TMP_SERVER_DIR = `${this.DIST_DIR}/tmp/${this.APP_SERVER}`;
+
+  /**
+   * The folder for the built files, corresponding to the current environment.
+   * @type {string}
+   */
+  APP_SERVER_DEST = this.BUILD_TYPE === BUILD_TYPES.DEVELOPMENT ? this.DEV_SERVER_DEST : this.PROD_SERVER_DEST;
+
+  /**
+   * The path for the base of the application at runtime.
+   * The default path is based on the environment '/',
+   * which can be overriden by the `--base` flag when running `npm start`.
+   * @type {string}
+   */
+  APP_SERVER_BASE = argv['base'] || '/api/';
+
+
+}
