d5ddb0bf566716a1188b11feb57715b1cfae7d14
Thavarajan <Thavarajan@gmail.com>
2017-02-19 20:38:35 +0530

    add the express inside the base routes

--- src/server/config/routes/base/BaseRoutes.ts
+++ src/server/config/routes/base/BaseRoutes.ts
@@ -1,13 +1,13 @@
 import express = require('express');
 import HeroRoutes = require('./../HeroRoutes');
 import SpartanRoutes = require('./../SpartanRoutes');
-var app = express();
 class BaseRoutes {
-    api='api';
-    get routes() {
-        app.use(`/${this.api}/`, new HeroRoutes().routes);
-        app.use(`/${this.api}/`, new SpartanRoutes().routes);
-        return app;
-    }
+  api = 'api';
+  get routes() {
+    var app = express();
+    app.use(`/${this.api}/`, new HeroRoutes().routes);
+    app.use(`/${this.api}/`, new SpartanRoutes().routes);
+    return app;
+  }
 }
 export = BaseRoutes;
