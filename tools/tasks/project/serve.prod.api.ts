c04e606b595a4ffec50395c86ae749197291442d
Thavarajan <Thavarajan@gmail.com>
2017-02-22 21:56:24 +0530

    rename and updated some work around for production

--- tools/tasks/project/serve.prod.api.ts
+++ tools/tasks/project/serve.prod.api.ts
@@ -1,15 +1,13 @@
 //import { serveAPI } from '../../../src/server/index';
 import { Apiserver } from './express-api';
 import { Task } from '../task';
-import Middlewares = require('../../../src/server/config/middlewares/base/MiddlewaresBase');
 export =
   function run() {
-    let app = Apiserver.API_APP;
+    let app = Apiserver.getApiconfig();
     var port = parseInt(process.env.PORT, 10) || 5000;
     console.log('port :' + port);
     app.set('port', port);
-    app.use(Middlewares.configuration);
-    Apiserver.server = app.listen(port, () => {
+    app.listen(port, () => {
       console.log('Mongo Api server is running at localhost:' + port);
     });
   };
