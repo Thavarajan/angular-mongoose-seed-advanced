e8cab320dac623555bdaf6c944be9bab9cb8b43c
Thavarajan <Thavarajan@gmail.com>
2017-02-22 21:57:18 +0530

    created a api class for api reference

--- tools/tasks/project/express-api.ts
+++ tools/tasks/project/express-api.ts
@@ -1,7 +1,13 @@
 import express = require('express');
 import { Server } from 'http';
+import Middlewares = require('../../../src/server/config/middlewares/base/MiddlewaresBase');
 
 export class Apiserver {
-  public static API_APP = express();
-  public static server: Server;
+  public static ApIserver: NodeJS.Process;
+
+  public static getApiconfig(): any {
+    let API_APP = express();
+    API_APP.use(Middlewares.configuration);
+    return API_APP;
+  }
 }
